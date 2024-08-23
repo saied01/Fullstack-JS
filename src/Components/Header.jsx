cfcbde9863c7acbda62f5b53a6162a1bfec4e085
saied01 <142250967+saied01@users.noreply.github.com>
2024-08-23 15:22:35 -0300

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = (props) => {
+    return (
+        <h1> {props.titulo} </h1>
+    )
+}
+
+export default Header;
---
+++ src/Components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = (total) => {
+    return (
+        <p> Number of exercises {total.total}</p>
+    )
+}
+
+export default Total;
